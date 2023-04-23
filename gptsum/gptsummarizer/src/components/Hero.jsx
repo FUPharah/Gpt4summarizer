f094e13631cbe611cf79f771178e18ddaf1ec9cb
canozben <canozben@windowslive.com>
2023-04-23 11:56:02 +0300

    UI nearly done

--- gptsum/gptsummarizer/src/components/Hero.jsx
+++ gptsum/gptsummarizer/src/components/Hero.jsx
@@ -4,12 +4,23 @@ import { logo } from '../assets'
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className= "flex justify-between items-center flex-col">
+      <nav className= "flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="logo" className="w-28 object-contain" />
-        <button type="button" onClick={() => window.open('https://github.com/FUPharah')} className="black_btn" >
+        <button type="button"
+        onClick={() => window.open('https://github.com/FUPharah')}
+        className="black_btn" >
           GitHub
         </button>
       </nav>
+
+      <h1 className="head_text">
+        Summarize whatever with <br className="max-md:hidden" />
+        <span className="orange_gradient">ChatGpt</span>
+      </h1>
+
+      <h2 className="desc">
+        This tool is a text summarizer that uses OpenAI GPT-4 to summarize your text for you.
+      </h2>
     </header>
   )
 }
