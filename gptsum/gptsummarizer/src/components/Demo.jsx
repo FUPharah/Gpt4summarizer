4cd7eaeffc5a0f01872c94a190b42934ac70a8c9
canozben <canozben@windowslive.com>
2023-04-23 15:16:21 +0300

    final touches

--- gptsum/gptsummarizer/src/components/Demo.jsx
+++ gptsum/gptsummarizer/src/components/Demo.jsx
@@ -119,7 +119,7 @@ const Demo = () => {
           <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
         ) : error ? (
           <p className='font-inter font-bold text-black text-center'>
-            Something is wrong
+            Something is wrong with the link. Please try again.
             <br />
             <span className='font-satoshi font-normal text-gray-700'>
               {error?.data?.error}
